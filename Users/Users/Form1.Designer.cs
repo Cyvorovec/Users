56421ace4420bf4716e19277c4b57ef7cccf69c7
Cyvorovec <57665709+Cyvorovec@users.noreply.github.com>
2021-06-09 00:34:49 +0300

    Add files via upload

---
+++ Users/Users/Form1.Designer.cs
@@ -0,0 +1,393 @@
+﻿
+namespace Test
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBoxFioAdd = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxLogAdd = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxLogDel = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxIDSearch = new System.Windows.Forms.TextBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.textBoxLogUpd = new System.Windows.Forms.TextBox();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.textBoxFioUpd = new System.Windows.Forms.TextBox();
+            this.Add = new System.Windows.Forms.Button();
+            this.Del = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.textBoxFioDel = new System.Windows.Forms.TextBox();
+            this.Upd = new System.Windows.Forms.Button();
+            this.Load = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FullName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Login = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Date = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(0, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(204, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Добавление пользователя";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(210, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(181, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Удаление пользователя";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label3.Location = new System.Drawing.Point(467, 18);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(308, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Изменение информации о пользователе";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label4.Location = new System.Drawing.Point(179, 333);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(163, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Выгрузить БД в excel";
+            // 
+            // textBoxFioAdd
+            // 
+            this.textBoxFioAdd.Location = new System.Drawing.Point(54, 58);
+            this.textBoxFioAdd.Name = "textBoxFioAdd";
+            this.textBoxFioAdd.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFioAdd.TabIndex = 4;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(13, 58);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(34, 15);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "ФИО";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(7, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(41, 15);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Логин";
+            // 
+            // textBoxLogAdd
+            // 
+            this.textBoxLogAdd.Location = new System.Drawing.Point(54, 97);
+            this.textBoxLogAdd.Name = "textBoxLogAdd";
+            this.textBoxLogAdd.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLogAdd.TabIndex = 7;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(204, 105);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 15);
+            this.label7.TabIndex = 8;
+            this.label7.Text = "Логин";
+            // 
+            // textBoxLogDel
+            // 
+            this.textBoxLogDel.Location = new System.Drawing.Point(251, 97);
+            this.textBoxLogDel.Name = "textBoxLogDel";
+            this.textBoxLogDel.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLogDel.TabIndex = 9;
+            // 
+            // label9
+            // 
+            this.label9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label9.Location = new System.Drawing.Point(467, 54);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(220, 35);
+            this.label9.TabIndex = 12;
+            this.label9.Text = "Идентификатор на обновление";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(473, 100);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(18, 15);
+            this.label10.TabIndex = 14;
+            this.label10.Text = "ID";
+            // 
+            // textBoxIDSearch
+            // 
+            this.textBoxIDSearch.Location = new System.Drawing.Point(514, 92);
+            this.textBoxIDSearch.Name = "textBoxIDSearch";
+            this.textBoxIDSearch.Size = new System.Drawing.Size(100, 23);
+            this.textBoxIDSearch.TabIndex = 13;
+            // 
+            // label11
+            // 
+            this.label11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label11.Location = new System.Drawing.Point(467, 134);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(116, 21);
+            this.label11.TabIndex = 15;
+            this.label11.Text = "Изменяем логин";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(466, 166);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(41, 15);
+            this.label12.TabIndex = 16;
+            this.label12.Text = "Логин";
+            // 
+            // textBoxLogUpd
+            // 
+            this.textBoxLogUpd.Location = new System.Drawing.Point(514, 158);
+            this.textBoxLogUpd.Name = "textBoxLogUpd";
+            this.textBoxLogUpd.Size = new System.Drawing.Size(100, 23);
+            this.textBoxLogUpd.TabIndex = 17;
+            // 
+            // label13
+            // 
+            this.label13.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label13.Location = new System.Drawing.Point(466, 197);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(116, 21);
+            this.label13.TabIndex = 18;
+            this.label13.Text = "Изменяем ФИО";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(464, 229);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(34, 15);
+            this.label14.TabIndex = 20;
+            this.label14.Text = "ФИО";
+            // 
+            // textBoxFioUpd
+            // 
+            this.textBoxFioUpd.Location = new System.Drawing.Point(505, 221);
+            this.textBoxFioUpd.Name = "textBoxFioUpd";
+            this.textBoxFioUpd.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFioUpd.TabIndex = 19;
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(13, 134);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(141, 23);
+            this.Add.TabIndex = 21;
+            this.Add.Text = "Добавить";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Del
+            // 
+            this.Del.Location = new System.Drawing.Point(204, 146);
+            this.Del.Name = "Del";
+            this.Del.Size = new System.Drawing.Size(141, 23);
+            this.Del.TabIndex = 22;
+            this.Del.Text = "Удалить";
+            this.Del.UseVisualStyleBackColor = true;
+            this.Del.Click += new System.EventHandler(this.Del_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(210, 58);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(34, 15);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "ФИО";
+            // 
+            // textBoxFioDel
+            // 
+            this.textBoxFioDel.Location = new System.Drawing.Point(251, 58);
+            this.textBoxFioDel.Name = "textBoxFioDel";
+            this.textBoxFioDel.Size = new System.Drawing.Size(100, 23);
+            this.textBoxFioDel.TabIndex = 23;
+            // 
+            // Upd
+            // 
+            this.Upd.Location = new System.Drawing.Point(464, 262);
+            this.Upd.Name = "Upd";
+            this.Upd.Size = new System.Drawing.Size(141, 23);
+            this.Upd.TabIndex = 25;
+            this.Upd.Text = "Обновить";
+            this.Upd.UseVisualStyleBackColor = true;
+            this.Upd.Click += new System.EventHandler(this.Upd_Click);
+            // 
+            // Load
+            // 
+            this.Load.Location = new System.Drawing.Point(190, 362);
+            this.Load.Name = "Load";
+            this.Load.Size = new System.Drawing.Size(141, 23);
+            this.Load.TabIndex = 26;
+            this.Load.Text = "Выгрузить";
+            this.Load.UseVisualStyleBackColor = true;
+            this.Load.Click += new System.EventHandler(this.Load_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ID,
+            this.FullName,
+            this.Login,
+            this.Date});
+            this.dataGridView1.Location = new System.Drawing.Point(13, 180);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 25;
+            this.dataGridView1.Size = new System.Drawing.Size(445, 150);
+            this.dataGridView1.TabIndex = 27;
+            // 
+            // ID
+            // 
+            this.ID.HeaderText = "ID";
+            this.ID.Name = "ID";
+            // 
+            // FullName
+            // 
+            this.FullName.HeaderText = "FullName";
+            this.FullName.Name = "FullName";
+            // 
+            // Login
+            // 
+            this.Login.HeaderText = "Login";
+            this.Login.Name = "Login";
+            // 
+            // Date
+            // 
+            this.Date.HeaderText = "Date";
+            this.Date.Name = "Date";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 397);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.Load);
+            this.Controls.Add(this.Upd);
+            this.Controls.Add(this.label15);
+            this.Controls.Add(this.textBoxFioDel);
+            this.Controls.Add(this.Del);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.label14);
+            this.Controls.Add(this.textBoxFioUpd);
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.textBoxLogUpd);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.textBoxIDSearch);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBoxLogDel);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxLogAdd);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFioAdd);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Добавление пользователя";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBoxFioAdd;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxLogAdd;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxLogDel;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxIDSearch;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.TextBox textBoxLogUpd;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.TextBox textBoxFioUpd;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Del;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.TextBox textBoxFioDel;
+        private System.Windows.Forms.Button Upd;
+        private System.Windows.Forms.Button Load;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ID;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FullName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Login;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Date;
+    }
+}
+
---
+++ Users/Users/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SQLite;
+using ClosedXML.Excel;
+
+namespace Test
+{
+    public partial class Form1 : Form
+    {
+        string path = "Users.db";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void data_show()
+        {
+            SQLiteConnection DB = new SQLiteConnection("Data Source=Users.db ; Version = 3");
+            DB.Open();
+            dataGridView1.Rows.Clear();
+            string com = "select ID, FullName, Login, Date from Users where DeleteFlag = 0";
+            SQLiteCommand cmd = new SQLiteCommand(com, DB);
+            SQLiteDataReader DR = cmd.ExecuteReader();
+
+            while (DR.Read())
+            {
+                dataGridView1.Rows.Insert(0, DR.GetInt32(0), DR.GetString(1), DR.GetString(2), DR.GetString(3));
+            }
+            DB.Close();
+        }
+
+        private void Create_DB()
+        {
+            if (!System.IO.File.Exists(path))
+            {
+                SQLiteConnection.CreateFile(path);
+                using (var sqlite = new SQLiteConnection(@"Data Source = " + path))
+                {
+                    sqlite.Open();
+                    string com = "CREATE TABLE Users(ID INTEGER NOT NULL UNIQUE,FullName TEXT,Login TEXT UNIQUE,Date REAL,DeleteFlag  INTEGER,PRIMARY KEY (ID AUTOINCREMENT))";
+                    SQLiteCommand cmd = new SQLiteCommand(com, sqlite);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        protected override void OnLoad(EventArgs e)
+        {
+            Create_DB();
+            data_show();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+
+        }
+
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            if (textBoxFioAdd.Text != "" && textBoxLogAdd.Text != "")
+            {
+                SQLiteConnection DB = new SQLiteConnection("Data Source=Users.db ; Version = 3");
+                DB.Open();
+                DateTime rowdate = DateTime.Now;
+                string date = rowdate.ToString("yyyy-MM-dd");
+                SQLiteCommand command = DB.CreateCommand();
+                command.CommandText = "INSERT INTO Users(FullName, Login, Date, DeleteFlag) VALUES (@FullName, @Login, '" + date + "', 0)";
+                command.Parameters.Add("@FullName", System.Data.DbType.String).Value = textBoxFioAdd.Text.ToUpper();
+                command.Parameters.Add("@Login", System.Data.DbType.String).Value = textBoxLogAdd.Text.ToUpper();
+                command.ExecuteNonQuery();
+
+                data_show();
+
+
+                DB.Close();
+
+            }
+            else
+            {
+                MessageBox.Show("Введите ФИО и Логин!");
+            }
+        }
+
+
+        private void Load_Click(object sender, EventArgs e)
+        {
+            SQLiteConnection DB = new SQLiteConnection("Data Source=Users.db ; Version = 3");
+            DB.Open();
+            SQLiteDataAdapter DA = new SQLiteDataAdapter("select *from Users", DB);
+            DataTable DT = new DataTable();
+            DA.Fill(DT);
+            using (SaveFileDialog SFD = new SaveFileDialog() { Filter = "Excel Workbook|*.xlsx" })
+            {
+                if (SFD.ShowDialog() == DialogResult.OK)
+                {
+                    using (XLWorkbook workbook = new XLWorkbook())
+                    {
+                        workbook.Worksheets.Add(DT,"Users");
+                        workbook.SaveAs(SFD.FileName);
+                    }
+                }
+            }
+            DB.Close();
+        }
+
+        private void Del_Click(object sender, EventArgs e)
+        {
+            if (textBoxFioDel.Text != "" && textBoxLogDel.Text != "")
+            {
+                SQLiteConnection DB = new SQLiteConnection("Data Source=Users.db ; Version = 3");
+                DB.Open();
+                SQLiteCommand command = DB.CreateCommand();
+                command.CommandText = "UPDATE Users Set DeleteFlag = 1 where Login =@Login and FullName=@FullName";
+                command.Parameters.Add("@FullName", System.Data.DbType.String).Value = textBoxFioDel.Text.ToUpper();
+                command.Parameters.Add("@Login", System.Data.DbType.String).Value = textBoxLogDel.Text.ToUpper();
+                command.ExecuteNonQuery();
+
+
+                data_show();
+
+                DB.Close();
+            }
+            else
+            {
+                MessageBox.Show("Введите ФИО и Логин!");
+            }
+        }
+
+        private void Upd_Click(object sender, EventArgs e)
+        {
+            if (textBoxIDSearch.Text != "")
+            {
+                SQLiteConnection DB = new SQLiteConnection("Data Source=Users.db ; Version = 3");
+                DB.Open();
+                SQLiteCommand com = DB.CreateCommand();
+                if (textBoxLogUpd.Text != "")
+                {
+                    com.CommandText = "UPDATE Users Set Login = @Login where Id =@Id";
+                    com.Parameters.Add("@Login", System.Data.DbType.String).Value = textBoxLogUpd.Text.ToUpper();
+                    com.Parameters.Add("@Id", System.Data.DbType.Int32).Value = textBoxIDSearch.Text;
+                    com.ExecuteNonQuery();
+
+                }
+                if (textBoxFioUpd.Text != "")
+                {
+                    com.CommandText = "UPDATE Users Set FullName = @FullName where Id =@Id";
+                    com.Parameters.Add("@FullName", System.Data.DbType.String).Value = textBoxFioUpd.Text.ToUpper();
+                    com.Parameters.Add("@Id", System.Data.DbType.Int32).Value = textBoxIDSearch.Text;
+                    com.ExecuteNonQuery();
+                }
+
+                if (string.IsNullOrEmpty(textBoxFioUpd.Text) && string.IsNullOrEmpty(textBoxLogUpd.Text))
+                {
+                    MessageBox.Show("Введите данные на обновление!");
+                }
+
+                data_show();
+                DB.Close();
+            }
+        }
+    }
+}
